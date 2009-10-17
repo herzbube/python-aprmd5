788c00bf5ff446b97c8b607634bc1da891aac2cd
Patrick Näf Moser <herzbube@herzbube.ch>
2009-10-17 15:28:08 +0200

    [feature] provide md5 object (preliminary but working version)

--- src/aprmd5.c
+++ src/aprmd5.c
@@ -37,6 +37,11 @@
 // included.
 #include <Python.h>
 
+// Defines PyMemberDef and PyMethodDef. Weird - why is this not in Python.h?
+// See http://bugs.python.org/issue2897.
+#include <structmember.h>
+
+
 // The filename of the header that we include depends on the version of
 // libaprutil that we want to use, and the platform we are on. We want this C
 // source file to remain independent, so we use a preprocessor macro and
@@ -49,6 +54,12 @@
 // improved so that the aprmd5 module returns md5 objects that encapsulate this
 // state.
 static apr_md5_ctx_t aprmd5_module_context;
+
+// The digest size is defined by libaprutil
+#define APRMD5_MD5_DIGESTSIZE   APR_MD5_DIGESTSIZE
+// The block size is *not* defined by libaprutil; assume the same value as in
+// Python's hashlib (see <pythonroot>/Modules/md5module.c)
+#define APRMD5_MD5_BLOCKSIZE    64
 
 
 // ---------------------------------------------------------------------------
@@ -391,11 +402,383 @@ aprmd5_md5_final(PyObject* self, PyObject* args)
   return Py_BuildValue("s#", hexDigest, hexDigestLen);
 }
 
+// ---------------------------------------------------------------------------
+// Definition of the C type that represents MD5 objects
+// ---------------------------------------------------------------------------
+
+typedef struct {
+	// Adds reference count and a pointer to the actual type object
+  PyObject_HEAD
+  // Type-specific fields go here
+  apr_md5_ctx_t context;  // keeps state between calls to init(), update()
+                          // and final()
+} aprmd5_md5Object;
+
+
+// ---------------------------------------------------------------------------
+// Allocation/deallocation of MD5 objects
+// ---------------------------------------------------------------------------
+
+// This function is responsible for creating objects *before* they are
+// initialized by obj.__init__(). It is exposed in Python as
+// class.__new__() method. __new__() is guaranteed to be called, even if the
+// object is unpickled.
+static PyObject *
+aprmd5_md5Object_new(PyTypeObject* type, PyObject* args, PyObject* kwds)
+{
+  fprintf(stdout, "new called\n");
+  aprmd5_md5Object* self = (aprmd5_md5Object*)type->tp_alloc(type, 0);
+  if (self != NULL)
+  {
+    apr_status_t status = apr_md5_init(&self->context);
+    if (status)
+    {
+      Py_DECREF(self);
+      return NULL;
+    }
+  }
+  return (PyObject*)self;
+}
+
+// This function is responsible for initializing objects *after* they have been
+// created by class.__new__(). It is exposed in Python as obj.__init__() method.
+// __init__() is not guaranteed to be called: it is not called when an object
+// is unpickled, and subclasses may also play dirty and not call __init__().
+static int
+aprmd5_md5Object_init(aprmd5_md5Object* self, PyObject* args, PyObject* kwds)
+{
+  fprintf(stdout, "init called, context is %d, digest_size = %d, block_size = %d\n", self->context, APRMD5_MD5_DIGESTSIZE, APRMD5_MD5_BLOCKSIZE);
+
+  // Make sure that no arguments have been passed
+  static char* kwlist[] = {NULL};
+  if (! PyArg_ParseTupleAndKeywords(args, kwds, "|", kwlist))
+    return -1;
+  if (0 == 0)   // TODO xxx replace this by some useful check
+  {
+    // We should never get here. If we do, aprmd5_md5Object_new() has not
+    // been called!
+    assert(false);
+    apr_status_t status = apr_md5_init(&self->context);
+    if (status)
+    {
+      PyErr_SetString(PyExc_RuntimeError, "apr_md5_init() returned status code != 0");
+      return -1;
+    }
+  }
+  return 0;
+}
+
+static void
+aprmd5_md5Object_dealloc(aprmd5_md5Object* self)
+{
+  // Free object memory; note that self might be a subclass instance (if we
+  // allow subclassing)
+#if PY_MAJOR_VERSION >= 3
+  Py_TYPE(self)->tp_free((PyObject*)self);
+#else   // #if PY_MAJOR_VERSION >= 3
+  self->ob_type->tp_free((PyObject*)self);
+#endif  // #if PY_MAJOR_VERSION >= 3
+}
+
+
+// ---------------------------------------------------------------------------
+// Implementation of methods of MD5 objects
+// ---------------------------------------------------------------------------
+
+// Forward declaration
+static PyTypeObject aprmd5_md5Type;
+
+// Helper
+static aprmd5_md5Object *
+new_md5Object(void)
+{
+  return (aprmd5_md5Object*)PyObject_New(aprmd5_md5Object, &aprmd5_md5Type);
+}
+
+static PyObject*
+aprmd5_md5Object_update(aprmd5_md5Object* self, PyObject* args)
+{
+  // TODO: We do this sizeof() check because later on, when we call
+  // apr_md5_update(), we are making an implicit type conversion from Py_ssize_t
+  // (inputLen) to apr_size_t (function argument type). Instead of aborting
+  // here, a better approach would probably be to iterate over the input buffer
+  // and call apr_md5_update() multiple times
+  assert(sizeof(apr_size_t) >= sizeof(Py_ssize_t));
+  if (sizeof(apr_size_t) < sizeof(Py_ssize_t))
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Function aborted, sizeof(apr_size_t) < sizeof(Py_ssize_t)");
+    return NULL;
+  }
+
+#if PY_MAJOR_VERSION >= 3
+  // Input must be a bytes() object from which we can get a char*
+  const char* format = "y#";
+#else
+  // Input must be a str() object from which we can get a char*. The string may
+  // contain null bytes.
+  const char* format = "s#";
+#endif
+  const char* input;
+  Py_ssize_t inputLen;
+  if (! PyArg_ParseTuple(args, format, &input, &inputLen))
+    return NULL;
+
+  // Feed the input into the MD5 algorithm
+  apr_status_t status = apr_md5_update(&self->context, input, inputLen);
+  if (status)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "apr_md5_update() returned status code != 0");
+    return NULL;
+  }
+
+  // Don't return NULL, as this would indicate an error
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+static PyObject*
+aprmd5_md5Object_digest(aprmd5_md5Object* self, PyObject* args)
+{
+  // Make a local copy of the context that we can operate on; apr_md5_final()
+  // will zero that copy, but the original state in self remains untouched so
+  // that the user can continue calling update()
+  apr_md5_ctx_t contextCopy = self->context;
+
+  // Generate the hash
+  unsigned char digest[APRMD5_MD5_DIGESTSIZE];
+  apr_status_t status = apr_md5_final(digest, &contextCopy);
+  if (status)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "apr_md5_final() returned status code != 0");
+    return NULL;
+  }
+
+  // Return the result; the Python system becomes responsible for the object
+  // returned by Py_BuildValue().
+  return Py_BuildValue("y#", digest, APRMD5_MD5_DIGESTSIZE);
+}
+
+static PyObject*
+aprmd5_md5Object_hexdigest(aprmd5_md5Object* self, PyObject* args)
+{
+  // Make a local copy of the context that we can operate on; apr_md5_final()
+  // will zero that copy, but the original state in self remains untouched so
+  // that the user can continue calling update()
+  apr_md5_ctx_t contextCopy = self->context;
+
+  // Generate the hash
+  unsigned char digest[APRMD5_MD5_DIGESTSIZE];
+  apr_status_t status = apr_md5_final(digest, &contextCopy);
+  if (status)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "apr_md5_final() returned status code != 0");
+    return NULL;
+  }
+
+  // Construct the hex digest from the binary digest
+  Py_ssize_t hexDigestLen = APRMD5_MD5_DIGESTSIZE * 2;
+  char hexDigest[hexDigestLen];
+  aprmd5_helper_bindigest_to_hexdigest(digest, hexDigest);
+
+  // Return the result; the Python system becomes responsible for the object
+  // returned by Py_BuildValue().
+  return Py_BuildValue("s#", hexDigest, hexDigestLen);
+}
+
+static PyObject*
+aprmd5_md5Object_copy(aprmd5_md5Object* self, PyObject* args)
+{
+  if (Py_TYPE(self) != &aprmd5_md5Type)
+    return NULL;
+  aprmd5_md5Object* newobj = new_md5Object();
+  if (NULL == newobj)
+    return NULL;
+  newobj->context = self->context;
+  return (PyObject *)newobj;
+
+  return NULL;
+}
+
+static PyObject *
+aprmd5_md5Object_get_digest_size(aprmd5_md5Object* self, void* closure)
+{
+  return PyLong_FromLong(APRMD5_MD5_DIGESTSIZE);
+}
+
+static PyObject *
+aprmd5_md5Object_get_block_size(aprmd5_md5Object* self, void* closure)
+{
+  return PyLong_FromLong(APRMD5_MD5_BLOCKSIZE);
+}
+
+static PyObject *
+aprmd5_md5Object_get_name(aprmd5_md5Object* self, void* closure)
+{
+  return PyUnicode_FromStringAndSize("md5", 3);
+}
+
+
+// ---------------------------------------------------------------------------
+// Attributes and methods of MD5 objects
+// ---------------------------------------------------------------------------
+
+static PyMemberDef aprmd5_md5Object_members[] =
+{
+  {NULL}  // Sentinel
+};
+
+static PyGetSetDef aprmd5_md5Object_getseters[] =
+{
+  {
+    "digest_size",
+    (getter)aprmd5_md5Object_get_digest_size, NULL,
+    "The size of the resulting hash in bytes.",
+    NULL
+  },
+  {
+    "block_size",
+    (getter)aprmd5_md5Object_get_block_size, NULL,
+    "The internal block size of the hash algorithm in bytes.",
+    NULL
+  },
+  {
+    "name",
+    (getter)aprmd5_md5Object_get_name, NULL,
+    NULL,
+    NULL
+  },
+  {NULL}  /* Sentinel */
+};
+
+static PyMethodDef aprmd5_md5Object_methods[] =
+{
+  {
+    "update", (PyCFunction)aprmd5_md5Object_update, METH_VARARGS,
+    "Update the hash object with the object arg, which must be interpretable as a buffer of bytes. Repeated calls are equivalent to a single call with the concatenation of all the arguments: m.update(a); m.update(b) is equivalent to m.update(a+b)."
+  },
+  {
+    "digest", (PyCFunction)aprmd5_md5Object_digest, METH_NOARGS,
+    "Return the digest of the data passed to the update() method so far. This is a bytes array of size digest_size which may contain bytes in the whole range from 0 to 255."
+  },
+  {
+    "hexdigest", (PyCFunction)aprmd5_md5Object_hexdigest, METH_NOARGS,
+    "Like digest() except the digest is returned as a string object of double length, containing only hexadecimal digits. This may be used to exchange the value safely in email or other non-binary environments."
+  },
+  {
+    "copy", (PyCFunction)aprmd5_md5Object_copy, METH_NOARGS,
+    "Return a copy (“clone”) of the hash object. This can be used to efficiently compute the digests of data sharing a common initial substring."
+  },
+  {NULL}  // Sentinel
+};
+
+// ---------------------------------------------------------------------------
+// Definition of the Python type that represents MD5 objects
+// ---------------------------------------------------------------------------
+
+#if PY_MAJOR_VERSION >= 3
+
+static PyTypeObject aprmd5_md5Type =
+{
+  PyObject_HEAD_INIT(NULL)
+  "aprmd5.md5",                  // tp_name
+  sizeof(aprmd5_md5Object),      // tp_basicsize
+  0,                             // tp_itemsize
+  (destructor)
+    aprmd5_md5Object_dealloc,    // tp_dealloc
+  0,                             // tp_print
+  0,                             // tp_getattr
+  0,                             // tp_setattr
+  0,                             // tp_reserved
+  0,                             // tp_repr
+  0,                             // tp_as_number
+  0,                             // tp_as_sequence
+  0,                             // tp_as_mapping
+  0,                             // tp_hash
+  0,                             // tp_call
+  0,                             // tp_str
+  0,                             // tp_getattro
+  0,                             // tp_setattro
+  0,                             // tp_as_buffer
+  Py_TPFLAGS_DEFAULT,            // tp_flags; Py_TPFLAGS_DEFAULT enables all
+                                 // members defined by the version of Python
+                                 // that this is compiled for
+  "md5 objects",                 // tp_doc
+  0,                             // tp_traverse
+  0,                             // tp_clear
+  0,                             // tp_richcompare
+  0,                             // tp_weaklistoffset
+  0,                             // tp_iter
+  0,                             // tp_iternext
+  aprmd5_md5Object_methods,      // tp_methods
+  aprmd5_md5Object_members,      // tp_members
+  aprmd5_md5Object_getseters,    // tp_getset
+  0,                             // tp_base
+  0,                             // tp_dict
+  0,                             // tp_descr_get
+  0,                             // tp_descr_set
+  0,                             // tp_dictoffset
+  (initproc)
+    aprmd5_md5Object_init,       // tp_init
+  0,                             // tp_alloc
+  aprmd5_md5Object_new,          // tp_new
+};
+
+#else   // #if PY_MAJOR_VERSION >= 3
+
+static PyTypeObject aprmd5_md5Type =
+{
+  PyObject_HEAD_INIT(NULL)
+  0,                             // ob_size
+  "aprmd5.md5",                  // tp_name
+  sizeof(aprmd5_md5Object),      // tp_basicsize
+  0,                             // tp_itemsize
+  (destructor)
+    aprmd5_md5Object_dealloc,    // tp_dealloc
+  0,                             // tp_print
+  0,                             // tp_getattr
+  0,                             // tp_setattr
+  0,                             // tp_compare
+  0,                             // tp_repr
+  0,                             // tp_as_number
+  0,                             // tp_as_sequence
+  0,                             // tp_as_mapping
+  0,                             // tp_hash
+  0,                             // tp_call
+  0,                             // tp_str
+  0,                             // tp_getattro
+  0,                             // tp_setattro
+  0,                             // tp_as_buffer
+  Py_TPFLAGS_DEFAULT,            // tp_flags; Py_TPFLAGS_DEFAULT enables all
+                                 // members defined by the version of Python
+                                 // that this is compiled for
+  "md5 objects",                 // tp_doc
+  0,                             // tp_traverse
+  0,                             // tp_clear
+  0,                             // tp_richcompare
+  0,                             // tp_weaklistoffset
+  0,                             // tp_iter
+  0,                             // tp_iternext
+  aprmd5_md5Object_methods,      // tp_methods
+  aprmd5_md5Object_members,      // tp_members
+  aprmd5_md5Object_getseters,    // tp_getset
+  0,                             // tp_base
+  0,                             // tp_dict
+  0,                             // tp_descr_get
+  0,                             // tp_descr_set
+  0,                             // tp_dictoffset
+  (initproc)
+    aprmd5_md5Object_init,       // tp_init
+  0,                             // tp_alloc
+  aprmd5_md5Object_new,          // tp_new
+};
+
+#endif  // #if PY_MAJOR_VERSION >= 3
+
 
 // ---------------------------------------------------------------------------
 // The method table: List methods in this module.
 // ---------------------------------------------------------------------------
-static PyMethodDef AprMD5Methods[] =
+static PyMethodDef aprmd5_methods[] =
 {
   {
     "md5_encode", aprmd5_md5_encode, METH_VARARGS,
@@ -431,14 +814,14 @@ static PyMethodDef AprMD5Methods[] =
 // ---------------------------------------------------------------------------
 #if PY_MAJOR_VERSION >= 3
 
-static struct PyModuleDef aprmd5module =
+static struct PyModuleDef aprmd5_module =
 {
   PyModuleDef_HEAD_INIT,
   "aprmd5",     // name of module
   NULL,         // module documentation, may be NULL
   -1,           // size of per-interpreter state of the module
                 // or -1 if the module keeps state in global variables
-  AprMD5Methods
+  aprmd5_methods
 };
 
 #endif  // #if PY_MAJOR_VERSION >= 3
@@ -456,7 +839,20 @@ PyMODINIT_FUNC
 
 PyInit_aprmd5(void)
 {
-  return PyModule_Create(&aprmd5module);
+  // Allows new objects to be created
+  aprmd5_md5Type.tp_new = PyType_GenericNew;
+  // Initializes the type
+  if (PyType_Ready(&aprmd5_md5Type) < 0)
+    return NULL;
+
+  PyObject* module = PyModule_Create(&aprmd5_module);
+  if (NULL == module)
+    return NULL;
+
+  Py_INCREF(&aprmd5_md5Type);
+  PyModule_AddObject(module, "md5", (PyObject*)&aprmd5_md5Type);
+
+  return module;
 }
 
 
@@ -464,7 +860,18 @@ PyInit_aprmd5(void)
 
 initaprmd5(void)
 {
-  (void) Py_InitModule("aprmd5", AprMD5Methods);
+  // Allows new objects to be created
+  aprmd5_md5Type.tp_new = PyType_GenericNew;
+  // Initializes the type
+  if (PyType_Ready(&aprmd5_md5Type) < 0)
+    return;
+
+  PyObject* module = Py_InitModule("aprmd5", aprmd5_methods);
+  if (NULL == module)
+    return;
+
+  Py_INCREF(&aprmd5_md5Type);
+  PyModule_AddObject(module, "md5", (PyObject*)&aprmd5_md5Type);
 }
 
 
